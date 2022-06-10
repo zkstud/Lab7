1e98f3b205d65a86409223e191beea5b5c975d17
ZAKHAR <37652@studentwsei.pl>
2022-06-10 14:02:08 +0200

    rectangle porusza sie razem z mysza

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class Controller {
     @FXML
@@ -13,6 +15,7 @@ public class Controller {
     @FXML
     public Pane pnArea;
     private boolean dragged = false;
+    private Rectangle rect;
     //    private EventHandler<MouseEvent> myClickHandler = new EvHandlerMouseClicked(this);
 //    private EventHandler<MouseEvent> myClickHandler = new EventHandler<MouseEvent>() {
 //        @Override
@@ -34,8 +37,16 @@ public class Controller {
         pnArea.setOnMouseClicked((MouseEvent mouseEvent) -> handleMouse(mouseEvent, true));
         pnArea.setOnMouseMoved((MouseEvent mouseEvent) -> handleMouse(mouseEvent, false));
         pnArea.setOnDragDetected((MouseEvent mouseEvent) -> dragged = true);
-        pnArea.setOnMouseDragged((MouseEvent mouseEvent) -> handleMouse(mouseEvent, false));
+        pnArea.setOnMouseDragged((MouseEvent mouseEvent) -> handleDrag(mouseEvent));
         pnArea.setOnMouseReleased((MouseEvent mouseEvent) -> dragged = false);
+
+        double w = pnArea.getWidth();
+        double h = pnArea.getHeight();
+        rect = new Rectangle(10.0, 10.0, Color.ALICEBLUE);
+        pnArea.getChildren().add(rect);
+        rect.setRotate(45);
+        rect.setWidth(50);
+        rect.setHeight(50);
     }
 
 
@@ -44,6 +55,13 @@ public class Controller {
         String prefix = dragged ? (aClick ? "" : "@") : (aClick ? "!" : "*");
         lbX.setText(prefix + mouseEvent.getX());
         lbY.setText(prefix + mouseEvent.getY());
+    }
+
+    public void handleDrag(MouseEvent mouseEvent){
+        lbX.setText("@" + mouseEvent.getX());
+        lbY.setText("@" + mouseEvent.getY());
+        rect.setX(mouseEvent.getX());
+        rect.setY(mouseEvent.getY());
     }
 
 }
